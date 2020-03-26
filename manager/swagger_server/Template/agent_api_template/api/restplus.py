c58e2ebdae5c4042d577222a1edffc617b314ab5
Swarmourr <safrihamza.ensasafi@gmail.com>
2020-03-26 17:01:01 +0100

    monitoriong before new updates

--- manager/swagger_server/Template/agent_api_template/api/restplus.py
+++
@@ -1,4 +0,0 @@
-from flask_restplus import Api
-
-api = Api(version='1.0', title='monitoring', description='Agent Monitoring ')
-# api.__schema__
--- manager/swagger_server/Template/agent_api_template/settings.py
+++
@@ -1,8 +0,0 @@
-# Flask settings
-FLASK_DEBUG = True  # Do not use debug mode in production
-
-# Flask-Restplus settings
-RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
-RESTPLUS_VALIDATE = False
-RESTPLUS_MASK_SWAGGER = False
-RESTPLUS_ERROR_404_HELP = False
